28436dc6dbcc4f0e1943dc188d6a216cbdda2fa1
Omar Araby <72764839+OmarAraby1@users.noreply.github.com>
2022-12-16 21:32:22 +0200

    Base file

---
+++ RegisterPage.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <header>
+        <title>HAAARK!</title>
+        <link rel="stylesheet" type="text/css" href="session1.css">
+    </header>
+    <body>
+
+    </body>    
+</html>
